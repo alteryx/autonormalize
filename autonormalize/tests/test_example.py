b1d00395d0a746e0a5c8356dc3abff5b87537f29
Allison Portis <allison.portis@featurelabs.com>
2019-07-31 13:21:18 -0400

    make lint

--- autonormalize/tests/test_example.py
+++ autonormalize/tests/test_example.py
@@ -1,11 +1,10 @@
-import sys, os
+import os  # noqa isort:skip
+import sys  # noqa isort:skip
+sys.path.append(os.path.abspath(os.path.join('..', '..',)))  # noqa
 
-sys.path.append(os.path.abspath(os.path.join('..', '..',)))
+import featuretools as ft  # noqa isort:skip
 
-import pandas as pd
-import featuretools as ft
-
-import autonormalize as an
+import autonormalize as an  # noqa isort:skip
 
 
 def test_ft_mock_customer():
@@ -15,7 +14,7 @@ def test_ft_mock_customer():
     entityset = an.auto_entityset(df, name="Customer Transactions", time_index='transaction_time')
 
     assert set(entityset['transaction_id'].df.columns) == set(['transaction_id', 'session_id', 'transaction_time',
-                                                           'product_id', 'amount'])
+                                                              'product_id', 'amount'])
 
     assert set(entityset['product_id'].df.columns) == set(['product_id', 'brand'])
 
@@ -26,4 +25,3 @@ def test_ft_mock_customer():
     assert set([str(rel) for rel in entityset.relationships]) == set(['<Relationship: transaction_id.session_id -> session_id.session_id>',
                                                                       '<Relationship: transaction_id.product_id -> product_id.product_id>',
                                                                       '<Relationship: session_id.customer_id -> customer_id.customer_id>'])
-
