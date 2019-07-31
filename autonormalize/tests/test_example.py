9f92abbbe50832cce76d290bfccfc948330de1ac
Allison Portis <allison.portis@featurelabs.com>
2019-07-31 12:12:54 -0400

    ft mock customer example test

--- autonormalize/tests/test_example.py
+++ autonormalize/tests/test_example.py
@@ -1,9 +1,29 @@
-# import os
+import sys, os
 
-# import pandas as pd
+sys.path.append(os.path.abspath(os.path.join('..', '..',)))
 
-# liquor_df = pd.read_csv(os.path.join(os.getcwd(), 'autonormalize/downloads/liquor.csv'))
-# liquor_df = liquor_df.drop(columns=liquor_df.columns[12:])
-# liquor_df = liquor_df.drop(range(3000000, 12591077))
-# liquor_df = liquor_df.dropna()
-# liquor_df = liquor_df.drop_duplicates()
+import pandas as pd
+import featuretools as ft
+
+import autonormalize as an
+
+
+def test_ft_mock_customer():
+    df = ft.demo.load_mock_customer(n_customers=80, n_products=50, n_sessions=200,
+                                    n_transactions=10000, return_single_table=True)
+
+    entityset = an.auto_entityset(df, name="Customer Transactions", time_index='transaction_time')
+
+    assert set(entityset['transaction_id'].df.columns) == set(['transaction_id', 'session_id', 'transaction_time',
+                                                           'product_id', 'amount'])
+
+    assert set(entityset['product_id'].df.columns) == set(['product_id', 'brand'])
+
+    assert set(entityset['session_id'].df.columns) == set(['session_id', 'customer_id', 'device', 'session_start'])
+
+    assert set(entityset['customer_id'].df.columns) == set(['customer_id', 'zip_code', 'join_date', 'date_of_birth'])
+
+    assert set([str(rel) for rel in entityset.relationships]) == set(['<Relationship: transaction_id.session_id -> session_id.session_id>',
+                                                                      '<Relationship: transaction_id.product_id -> product_id.product_id>',
+                                                                      '<Relationship: session_id.customer_id -> customer_id.customer_id>'])
+
