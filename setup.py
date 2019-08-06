1184162340034aa467cf0af3da62d53589e81432
Allison Portis <allison.portis@featurelabs.com>
2019-08-06 11:43:47 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,38 @@
+from os import path
+
 from setuptools import find_packages, setup
+
+dirname = path.abspath(path.dirname(__file__))
+with open(path.join(dirname, 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name='autonormalize',
     version='0.0.0',
+    description='a library for automated table normalization',
+    url='http://featuretools.com',
+    license='BSD 3-clause',
     author='Feature Labs, Inc.',
     author_email='support@featurelabs.com',
-    license='BSD 3-clause',
-    url='http://www.featurelabs.com/',
+    classifiers=[
+         'Development Status :: 3 - Alpha',
+         'Intended Audience :: Developers',
+         'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3.5',
+         'Programming Language :: Python :: 3.6',
+         'Programming Language :: Python :: 3.7'
+    ],
     install_requires=open('requirements.txt').readlines(),
     tests_require=open('test-requirements.txt').readlines(),
+    test_suite='autonormalize/tests',
     packages=find_packages(),
     include_package_data=True,
+    entry_points={
+        "featuretools_plugin": [
+            'autonormalize = autonormalize',
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
