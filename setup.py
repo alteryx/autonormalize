0b657f0d204b044843ad439e8898e0455deb5fd3
Allison Portis <51127819+allisonportis@users.noreply.github.com>
2019-08-16 15:13:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(dirname, 'README.md')) as f:
 
 setup(
     name='autonormalize',
-    version='1.0.0',
+    version='1.0.1',
     description='a library for automated table normalization',
     url='https://github.com/FeatureLabs/autonormalize',
     license='BSD 3-clause',
