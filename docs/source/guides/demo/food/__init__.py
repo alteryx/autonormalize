3cab06f2b04db32252141c5b63d28b606133c2ba
Ethan Tu <ethan.tu@alteryx.com>
2021-12-02 19:35:45 -0500

    download dataset for docs

---
+++ docs/source/guides/demo/food/__init__.py
@@ -0,0 +1,10 @@
+from demo import PWD
+from pandas import read_csv
+from os.path import join
+
+PWD = join(PWD, 'food')
+
+def load_sample():
+    food_df = read_csv(join(PWD, "FAO.csv"), encoding="latin1")
+    food_df = food_df.drop(columns=food_df.columns[10:])
+    return food_df
