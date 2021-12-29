260ae9a704d24868a3a658d139583b14c75251b4
Ethan Tu <ethan.tu@alteryx.com>
2021-12-29 13:28:49 -0500

    remove featuretool references

--- docs/make.bat
+++ docs/make.bat
@@ -129,9 +129,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\featuretools.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\autonormalize.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\featuretools.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\autonormalize.ghc
 	goto end
 )
 
