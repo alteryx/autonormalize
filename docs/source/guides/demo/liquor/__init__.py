1194347aa829bd80331ae7aa00165c6bcb0b7972
Ethan Tu <ethan.tu@alteryx.com>
2021-12-28 22:50:03 -0500

    add comment explaining reproducability code

--- docs/source/guides/demo/liquor/__init__.py
+++ docs/source/guides/demo/liquor/__init__.py
@@ -6,6 +6,7 @@ PWD = join(PWD, 'liquor')
 
 def load_sample():
     df = read_csv(join(PWD, "Iowa_Liquor_Sales.csv"))
+    # How to reproduce the sample from https://www.kaggle.com/residentmario/iowa-liquor-sales
     # df = df.drop(columns=df.columns[12:])
     # df = df.drop(range(1500000, 12591077))
     # df = df.dropna()
